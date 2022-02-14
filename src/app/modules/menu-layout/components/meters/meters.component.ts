0c7b63430b571142c68b79c66c3ba4eaa6721002
Jared Velasquez <jared.velasquez1122@gmail.com>
2022-02-14 16:39:34 -0600

    Avance medidores

--- src/app/modules/menu-layout/components/meters/meters.component.ts
+++ src/app/modules/menu-layout/components/meters/meters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MetersService } from "../../services/meters.service";
 interface ItemData {
   id: number;
   name: string;
@@ -12,19 +13,36 @@ interface ItemData {
   styleUrls: ['./meters.component.css']
 })
 export class MetersComponent implements OnInit {
-  dataList: Array<{ name: string; age: number; address: string }> = [];
+  
+  filterCodigo : Array<{text: string, value: any}> = [];
+  filterModel : Array<{text: string, value: any}> = [];
+  filterSerie : Array<{text: string, value: any}> = [];
+  dataList!: any;
   titleList: object[] = [{name: '#'}, {name: 'Codigo'}, {name: 'Descripcion'}, {name: 'Tipo'}, 
-    {name: 'Modelo'}, {name: 'Serie'}
+    {name: 'Modelo'}, {name: 'Serie'}, {name: 'Estado'}
   ];
+  constructor (
+    private metersService: MetersService
+  ){
+
+  }
 
   ngOnInit(): void {
-    for (let i = 0; i < 100; i++) {
-      this.dataList.push({
-        name: `Edward King`,
-        age: 32,
-        address: `LondonLondonLondonLondonLondon`
-      });
-    }
+    this.metersService.GetMeters().subscribe(
+      (result:any) => {
+        this.dataList = result;
+        
+        if(this.dataList){
+          for(let i = 0 ; i < this.dataList.length ; i++){
+            
+            this.filterModel.push({text: this.dataList[i].Modelo+'', value: this.dataList[i].Modelo});
+            this.filterSerie.push({text: this.dataList[i].Serie+'', value: this.dataList[i].Serie})
+            this.filterCodigo.push({text: this.dataList[i].Codigo+'', value: this.dataList[i].Codigo});
+          }
+
+        }
+      }
+    );
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,7 +9,9 @@ import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { MetersTableComponent } from './components/meters-table/meters-table.component';
-
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,10 @@ import { MetersTableComponent } from './components/meters-table/meters-table.com
     IconsProviderModule,
     NzLayoutModule,
     NzMenuModule,
-    NzTableModule
+    NzTableModule,
+    NzButtonModule,
+    NzInputModule,
+    NzGridModule
   ],
   exports: [
     MenuLayoutComponent,
