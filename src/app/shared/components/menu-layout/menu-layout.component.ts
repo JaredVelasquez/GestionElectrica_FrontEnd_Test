21f43f897892f853f28498594ee2b5665f2b4bdb
Jared Velasquez <jared.velasquez1122@gmail.com>
2022-06-03 15:53:53 -0600

    ajustes boton de retroceder

--- src/app/shared/components/menu-layout/menu-layout.component.ts
+++ src/app/shared/components/menu-layout/menu-layout.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-layout.component.css']
 })
 export class MenuLayoutComponent implements OnInit {
-  isCollapsed = false;
+  isCollapsed = true;
 
   deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30;
   constructor() { }
