7e3677e93b36d27a0dd46837b08f5fe47a39e9f3
Jared Velasquez <jared.velasquez1122@gmail.com>
2022-08-26 10:27:19 -0600

    Acceso a backend fuera de red

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment =  {
   production: true,
-  api:'http://svr-monitoreo:3000/'
+  api:'http://svr-monitoreo:3000/' || 'http://181.189.242.18:3000/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: 'http://localhost:3000/'
+  api: 'http://localhost:3000/' || 'http://181.189.242.18:3000/',
 };
 
 /*
